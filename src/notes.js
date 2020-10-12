d99bd8d56e97b368944a745637a991c54a53a8d0
Tanay Banerjee <banerjeetanay333@gmail.com>
2020-10-12 13:11:25 +0530

    committed

--- src/notes.js
+++ src/notes.js
@@ -24,10 +24,16 @@ const notes = [
       "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software.",
   },
   {
-    key: 4,
+    key: 5,
     title: 'Hardware vs. Software',
     content:
       "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software. Why did the programmer quit his job? A. Because he didn't get arrays. Why did the programmer quit his job? A. Because he didn't get arrays. Why did the programmer quit his job? A. Because he didn't get arrays. Why did the programmer quit his job? A. Because he didn't get arrays.",
+  },
+  {
+    key: 6,
+    title: 'Hardware vs. Software',
+    content:
+      "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software. Why did the programmer quit his job? A. Because he didn't get arrays. Why did the programmer quit his job? A. Because he didn't get arrays. Why did the programmer quit his job? A. Because he didn't get arrays. Why did the programmer quit his job? A. Because he didn't get arrays.What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software. Why did the programmer quit his job? A. Because he didn't get arrays. Why did the programmer quit his job? A. Because he didn't get arrays. Why did the programmer quit his job? A. Because he didn't get arrays. Why did the programmer quit his job? A. Because he didn't get arrays.",
   },
 ]
 
--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './homepage.styles.css'
+
 //importing components
 import Footer from '../../components/footer/footer'
 import Directory from '../../components/directory/directory'
